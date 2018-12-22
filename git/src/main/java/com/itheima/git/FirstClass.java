4441b4fb160de883a1c9e02f945e62527e95db97
ayi <ayi@itcast.cn>
2018-12-22 20:06:51 +0800

    提交本地工程

---
+++ git/src/main/java/com/itheima/git/FirstClass.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("曹志义");
+        System.out.println("曹志义");
+        System.out.println("曹志义");
+        System.out.println("曹志义");
+        System.out.println("曹志义");
+    }
+}
